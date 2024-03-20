5c30d64326008c220f1c4a30dd4d342cc3f13fd9
Javohir0080703 <renobobopocta@gmail.com>
2024-03-20 22:40:19 +0500

    fix regEX

--- script.js
+++ script.js
@@ -5,20 +5,39 @@ const imgInput = document.querySelector("#myfile");
 const data = [];
 
 function getRegister() {
-  const userData = {
-    name: inputs[0].value,
-    age: inputs[1].value,
-    phoneNumber: inputs[2].value,
-    email: inputs[3].value,
-    about: inputs[4].value,
-  };
-  data.push(userData);
+  const nameRegex = /^[a-zA-Z]{4,56}$/;
+  const ageRegex = /^[a-zA-Z]{4,56}$/;
+  const phoneNumberRegex = /^[0-9+]+$/;
+  const emailRegex = /^[a-z]$/;
+  const aboutRegex = /^[a-zA-Z]$/;
+
+  if (
+    nameRegex.test(inputs[0].value) &&
+    ageRegex.test(inputs[1].value) &&
+    phoneNumberRegex.test(inputs[2].value) &&
+    emailRegex.test(inputs[3].value) &&
+    aboutRegex.test(inputs[4].value)
+  ) {
+    const userData = {
+      name: capitalizeFirstLetter(inputs[0].value),
+      age: capitalizeFirstLetter(inputs[1].value),
+      phoneNumber: inputs[2].value,
+      email: inputs[3].value,
+      about: inputs[4].value,
+    };
+    data.push(userData);
+  } else {
+    alert(1);
+  }
 
   // Map qilish uchun nomlangan funktsiya
   const mappedData = mapData(data);
 
   // Ma'lumotlarni HTML-ga chiqaring
   displayData(mappedData);
+}
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function mapData(data) {
@@ -48,6 +67,19 @@ function displayData(mappedData) {
   const container = document.getElementById("data-container");
   container.innerHTML = "";
 
+  // mappedData ni name bo'yicha saralash
+  mappedData.sort((a, b) => {
+    const nameA = a.name.toLowerCase();
+    const nameB = b.name.toLowerCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+
   mappedData.forEach((user) => {
     const div = document.createElement("div");
 
@@ -62,14 +94,15 @@ function displayData(mappedData) {
     const phoneNumberElement = document.createElement("p");
     phoneNumberElement.innerText = `${user.phoneNumber}`;
     div.appendChild(phoneNumberElement);
-    // phoneNumberElement.classList.add("")
+
     container.appendChild(div);
   });
-  // (inputs[0].value = ""),
-  //   (inputs[1].value = ""),
-  //   (inputs[2].value = ""),
-  //   (inputs[3].value = ""),
-  //   (inputs[4].value = "");
+
+  inputs[0].value = "";
+  inputs[1].value = "";
+  inputs[2].value = "";
+  inputs[3].value = "";
+  inputs[4].value = "";
 }
 
 // regex
